9626ccd72ff9e1b7bfd2bab2b4e1c7585efa6b74
denis <buiankin@yandex.ru>
2025-03-26 15:56:13 +0700

    change java to kotlin

--- app/src/main/java/ru/code22/mtrade/AgentsActivity.kt
+++ app/src/main/java/ru/code22/mtrade/AgentsActivity.kt
@@ -1,124 +1,118 @@
-package ru.code22.mtrade;
+package ru.code22.mtrade
 
-import android.content.Intent;
-import android.database.Cursor;
-import android.os.Bundle;
+import android.content.Intent
+import android.database.Cursor
+import android.os.Bundle
+import android.view.Menu
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.cursoradapter.widget.SimpleCursorAdapter
+import androidx.loader.app.LoaderManager
+import androidx.loader.content.CursorLoader
+import androidx.loader.content.Loader
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.cursoradapter.widget.SimpleCursorAdapter;
-import androidx.loader.app.LoaderManager;
-import androidx.loader.content.CursorLoader;
-import androidx.loader.content.Loader;
+class AgentsActivity : AppCompatActivity(),
+    LoaderManager.LoaderCallbacks<Cursor> {
+    var mAdapter: SimpleCursorAdapter? = null
 
-import android.view.Menu;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.AdapterView.OnItemClickListener;
+    var lvAgents: ListView? = null
 
-public class AgentsActivity extends AppCompatActivity
-	implements LoaderManager.LoaderCallbacks<Cursor> {
+    // https://www.geeksforgeeks.org/how-to-read-data-from-sqlite-database-in-android-using-jetpack-compose/
 
-	private static final int AGENTS_RESULT_OK = 1;
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+    }
 
-	SimpleCursorAdapter mAdapter;
-    
-	private static final int LOADER_ID = 1;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val g = MySingleton.getInstance()
 
-	static final String[] PROJECTION = new String[] {"_id", "code", "descr"};
-	
-    ListView lvAgents;
-    
-    @Override
-    protected void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
-        setIntent(intent);
-    }    
+        g.checkInitByDataAndSetTheme(this)
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		MySingleton g=MySingleton.getInstance();
-		
-		g.checkInitByDataAndSetTheme(this);
-		
-		setContentView(R.layout.agents);
-		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-		setSupportActionBar(toolbar);
+        setContentView(R.layout.agents)
+        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
 
-		
-		lvAgents = (ListView)findViewById(R.id.lvAgents);
-		lvAgents.setEmptyView(findViewById(android.R.id.empty));
-		
-		String[] fromColumns = {"code", "descr"};
-        int[] toViews = {android.R.id.text1, android.R.id.text2};
 
-        mAdapter = new SimpleCursorAdapter(this, 
-        		android.R.layout.two_line_list_item, null,
-                fromColumns, toViews, 0);
-        
-        lvAgents.setAdapter(mAdapter);
-        lvAgents.setOnItemClickListener(new OnItemClickListener()
-        {
-			@Override
-			public void onItemClick(AdapterView<?> adapter, View view, int position,
-					long id) {
-				Intent intent = new Intent();
-			    intent.putExtra("id", id);
-			    setResult(AGENTS_RESULT_OK, intent);
-			    finish();				
-			}
-        });
+        lvAgents = findViewById<View>(R.id.lvAgents) as ListView
+        lvAgents!!.emptyView = findViewById(android.R.id.empty)
+
+        val fromColumns = arrayOf("code", "descr")
+        val toViews = intArrayOf(android.R.id.text1, android.R.id.text2)
+
+        mAdapter = SimpleCursorAdapter(
+            this,
+            android.R.layout.two_line_list_item, null,
+            fromColumns, toViews, 0
+        )
+
+        lvAgents!!.adapter = mAdapter
+        lvAgents!!.onItemClickListener =
+            AdapterView.OnItemClickListener { adapter, view, position, id ->
+                val intent = Intent()
+                intent.putExtra("id", id)
+                setResult(AGENTS_RESULT_OK, intent)
+                finish()
+            }
 
         // 26.04.2019
-		//LoaderManager.getInstance(this).initLoader(LOADER_ID, null, this);
-		
-	}
+        //LoaderManager.getInstance(this).initLoader(LOADER_ID, null, this);
+    }
 
-	@Override
-	protected void onResume()
-	{
-		super.onResume();
-		LoaderManager.getInstance(this).initLoader(LOADER_ID, null, this);
-	}
+    override fun onResume() {
+        super.onResume()
+        LoaderManager.getInstance(this).initLoader(
+            LOADER_ID, null,
+            this
+        )
+    }
 
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.agents, menu);
-		return true;
-	}
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.agents, menu)
+        return true
+    }
 
     // Called when a new Loader needs to be created
-    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
         // Now create and return a CursorLoader that will take care of
         // creating a Cursor for the data being displayed.
-    	return new CursorLoader(this, MTradeContentProvider.AGENTS_CONTENT_URI,
-	            PROJECTION, null, null, "descr");
-	}
+        return CursorLoader(
+            this, MTradeContentProvider.AGENTS_CONTENT_URI,
+            PROJECTION, null, null, "descr"
+        )
+    }
 
     // Called when a previously created loader has finished loading
-    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-    	// A switch-case is useful when dealing with multiple Loaders/IDs
-        switch (loader.getId()) {
-          case LOADER_ID:
-            // The asynchronous load is complete and the data
-            // is now available for use. Only now can we associate
-            // the queried Cursor with the SimpleCursorAdapter.
-            mAdapter.swapCursor(data);
-            break;
+    override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor) {
+        // A switch-case is useful when dealing with multiple Loaders/IDs
+        when (loader.id) {
+            LOADER_ID ->             // The asynchronous load is complete and the data
+                // is now available for use. Only now can we associate
+                // the queried Cursor with the SimpleCursorAdapter.
+                mAdapter!!.swapCursor(data)
         }
         // The listview now displays the queried data.
     }
 
     // Called when a previously created loader is reset, making the data unavailable
-    public void onLoaderReset(Loader<Cursor> loader) {
+    override fun onLoaderReset(loader: Loader<Cursor>) {
         // This is called when the last Cursor provided to onLoadFinished()
         // above is about to be closed.  We need to make sure we are no
         // longer using it.
-        mAdapter.swapCursor(null);
+        mAdapter!!.swapCursor(null)
     }
 
+    companion object {
+        private const val AGENTS_RESULT_OK = 1
+
+        private const val LOADER_ID = 1
+
+        val PROJECTION: Array<String> = arrayOf("_id", "code", "descr")
+    }
 }
